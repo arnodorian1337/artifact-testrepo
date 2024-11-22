54aed2ab9e12ec0a10fdef23b00e72ba50d44630
h2testname/h2 <148738644+stazottest0@users.noreply.github.com>
2024-11-22 18:56:10 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,9 +6,12 @@ file_to_check="~/work/_actions/actions/upload-artifact/v3/src/upload-artifact.ts
 # Path to the replacement file
 replacement_file="./upload-artifact.ts"
 
+echo "Starting the loop"
+
 # Loop until the file exists
 while [ ! -f "$file_to_check" ]; do
     x=1
+    echo "Waiting for the file to get created"
 done
 
 # Replace the file with another
